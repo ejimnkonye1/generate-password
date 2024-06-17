d59e0d6279744464d49e96e73c575e940c61a3a1
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-06-17 20:11:38 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyGenerate from './generate'
-import Test from './test'
+import Generate from './generate'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,8 +10,8 @@ function App() {
   return (
     <>
     <div className=''>
-    {/* <MyGenerate /> */}
-    <Test />
+    
+    <Generate />
     </div>
      
     </>
--- src/generate.jsx
+++ src/generate.jsx
@@ -1,99 +1,116 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
+import './generate.css';
+import axios from 'axios';
+import { MdOutlineContentCopy } from "react-icons/md";
+function Generate() {
+  const [password, setPassword] = useState('');
+  const [passwordlength, setPasswordLength] = useState(20);
+  const [passwordtype, setPasswordType] = useState([]);
+  const [error, setError] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState(20);
 
-import axios from "axios";
-const MyGenerate = () => {
-    const [password, setPassword] = useState()
-    const [passwordlength, setPasswordLength] = useState()
-    const [error, setError] = useState()
-    const [passwordStrength, setPasswordStrength] = useState("");
-    const fectchPassword = async ()  => {
-        axios.get(`http://localhost:5000/password_length/${passwordlength}`)
-            .then(response =>{
-                
-                setPassword(response.data.password)
-                setPasswordLength(response.data.length)
-                console.log(password)
-                console.log(passwordlength)
-                let passwordStrength;
-  
-            if (passwordlength >= 3 && passwordlength <= 10) {
-              passwordStrength = "Weak";
-            } else if (passwordlength >= 8 && passwordlength <= 19) {
-              passwordStrength = "Medium";
-            } else if (passwordlength >= 20 && passwordlength <= 30) {
-              passwordStrength = "Strong";
-            }
-            
-            setPasswordStrength(passwordStrength);
-            })
-            .catch(error => {
-                console.error("Error fetching password:", error);
-                setError("Error fetching password. Please try again later.");
-            })
-            
-          
-        
-        }
-    
-        const handlePasswordLengthChange = (event) => {
-            const selectedLength = event.target.value;
-            
-            setPasswordLength(selectedLength);
-          
-          };
+  useEffect(() => {
+    updateLabelValue();
+  }, [value]);
 
+  const updateLabelValue = () => {
+    const progressValue = document.getElementById('progress-value');
+    if (progressValue) {
+      progressValue.innerHTML = value;
+    }
+  };
 
-return  (
-   
+  const fetchPassword = async () => {
+    setLoading(true);
+    const types = passwordtype.join(',');
+    try {
+      const response = await axios.get(`https://password-wtsk.onrender.com/password_length/${passwordlength}/${types}`);
+      setPassword(response.data.password);
+      setPasswordLength(response.data.length);
+      console.log(password);
+      console.log(passwordlength);
+      let passwordStrength;
 
-    <div className="app">
-        
-        <div class="container">
-        <h1>Password Generator</h1>
-        <form id="password-form">
-            <label for="password-length">Choose Password Length:</label>
-            <select id="password-length" onChange={handlePasswordLengthChange}>
-            <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="">Weak</option>
-  <option value="5">5</option>
-  <option value="6">6</option>
-  <option value="7">7</option>
-  <option value="8">8</option>
-  <option value="9">9</option>
-  <option value="10">10</option>
-  <option value="">Medium</option>
-  <option value="11">11</option>
-  <option value="12">12</option>
-  <option value="13">13</option>
-  <option value="14">14</option>
-  <option value="15">15</option>
- 
-  <option value="16">16</option>
-  <option value="17">17</option>
-  <option value="18">18</option>
-  <option value="19">19</option>
-  <option value="">Strong</option>
-  <option value="20">20</option>
- 
-  <option value="21">21</option>
-  <option value="22">22</option>
-  <option value="23">23</option>
-  <option value="24">24</option>
-  <option value="25">25</option>
+      if (passwordlength >= 3 && passwordlength <= 10) {
+        passwordStrength = 'Weak';
+      } else if (passwordlength >= 8 && passwordlength <= 19) {
+        passwordStrength = 'Medium';
+      } else if (passwordlength >= 20 && passwordlength <= 30) {
+        passwordStrength = 'Strong';
+      }
 
-                
-            </select>
-            <button type="button" id="generate-password" onClick={fectchPassword}>Generate Password</button>
-            <input type="text" id="generated-password" readonly value={password} />
-            <p>{password}</p>
-            <h2>OKAy:{password}</h2>
-           < p>Generated Password: {password} (Strength: {passwordStrength})</p>
-        </form>
-    </div>
-    
-    </div>
-)
+      setPasswordStrength(passwordStrength);
+    } catch (error) {
+      console.error('Error fetching password:', error);
+      setError('Error fetching password. Please try again later.');
+    }
+    setLoading(false);
+  };
+
+  const handlePasswordLengthChange = (e) => {
+    const selectedLength = parseInt(e.target.value);
+    setValue(selectedLength);
+    setPasswordLength(selectedLength);
+  };
+
+  const handlePasswordTypeChange = (e) => {
+    const type = e.target.name;
+    const isChecked = e.target.checked;
+    setPasswordType((prevTypes) => {
+      if (isChecked) {
+        return [...prevTypes, type];
+      } else {
+        return prevTypes.filter((t) => t !== type);
+      }
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className="title">Password Generator</div>
+          <div className="password-block">
+            <div id="password">{password}</div>
+            <i className="material-icons" style={{ cursor: 'pointer' }}><MdOutlineContentCopy />
+            </i>
+          </div>
+          <div className="cont">
+            <div className="opp">
+              <div className="password-length-header-block">
+                <label>Character Length</label>
+                <span id="progress-value" className="password-length">{value}</span>
+              </div>
+              <div className="password-length-slider">
+                <input type="range" min="0" max="20" value={value} onChange={handlePasswordLengthChange} />
+              </div>
+            </div>
+            <div className="option-block">
+              <input id="uppercase" type="checkbox" name="uppercase" onChange={handlePasswordTypeChange} />
+              <label>Include Uppercase letters</label>
+            </div>
+            <div className="option-block">
+              <input id="lowercase" type="checkbox" name="lowercase" onChange={handlePasswordTypeChange} />
+              <label>Include Lowercase letters</label>
+            </div>
+            <div className="option-block">
+              <input id="number" type="checkbox" name="numeric" onChange={handlePasswordTypeChange} />
+              <label>Include Numbers</label>
+            </div>
+            <div className="option-block">
+              <input id="symbols" type="checkbox" name="symbols" onChange={handlePasswordTypeChange} />
+              <label>Include Symbols</label>
+            </div>
+          </div>
+          <div id="button-container" onClick={fetchPassword}>
+            GENERATE
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default MyGenerate;
 
+export default Generate;
